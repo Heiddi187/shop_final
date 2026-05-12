0fea956ee89236579b04298787319809b490a0a8
Heiddi187 <heiddi187@gmail.com>
2026-05-12 19:50:08 +0000

    supabase setup and testing function

--- shop/src/App.tsx
+++ shop/src/App.tsx
@@ -1,13 +1,48 @@
+import { supabase } from "./lib/supabase";
+import { useEffect, useState } from "react";
+import "./App.css";
 
-import './App.css'
+type Product = {
+   id: string;
+   shop_id?: string;
+   name: string;
+   slug?: string;
+   description?: string;
+   price_cents: number
+};
 
 function App() {
+   const [products, setProducts] = useState<Product[]>([]);
 
-  return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  )
+   useEffect(() => {
+      async function getProducts() {
+         const { data, error } = await supabase.from("products").select("*");
+
+         if (error) {
+            console.log(error);
+         } else {
+            setProducts(data);
+         }
+      }
+      getProducts();
+   }, []);
+
+   return (
+      <>
+         <h1>Hello world</h1>
+         <div>
+            <h1>Products</h1>
+
+            {products.map((product) => (
+               <div key={product.id}>
+                  <h2>{product.name}</h2>
+                  <p>{product.description}</p>
+                  <p>{product.price_cents}</p>
+               </div>
+            ))}
+         </div>
+      </>
+   );
 }
 
-export default App
+export default App;
